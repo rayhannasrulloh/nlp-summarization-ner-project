0dc49e96d779d71190631d45e12f52c48eeebb56
Rayhan Nasrulloh <raihan.rosidi354n@gmail.com>
2026-01-26 18:52:48 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+*** project nlp summarization ner ***
---
+++ web/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nlp summ ner project</title>
+</head>
+<body>
+
+    <h1>welcome to the NLP Summarization and Named Entity Recognition Project</h1>
+    <p>This project focuses on natural language processing techniques for text summarization and named entity recognition.</p>
+
+    
+</body>
+</html>
